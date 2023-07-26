03f4dfed49768f7f9b6a8c103380b5b39ca177ba
Samuel <samuelou510@gmail.com>
2023-07-26 12:46:13 -0400

    first method found

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        anagrams =[]
+        for string in list:
+            if(sorted(string) == sorted(self.word)):
+                anagrams.append(string)
+        return anagrams
+    pass
